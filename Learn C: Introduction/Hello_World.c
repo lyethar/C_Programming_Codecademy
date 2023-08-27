5bda97241cf845b39cedd9258a3c79d351b8bcd5
lyethar <107083757+lyethar@users.noreply.github.com>
2023-08-27 15:13:08 -0400

    Update Hello_World.c

--- Learn C: Introduction/Hello_World.c
+++ Learn C: Introduction/Hello_World.c
@@ -4,6 +4,6 @@ int main() {
   // <---- This is a comment
   /* This is a comment too  */
   // \t <--- tab \n <--- new line 
-  
+  // Compilation : gcc file.c -o out_prog
   printf("Hello World\n");
 }
